52e6ab51d41b149bf6c7367426cf9f5cf43c16b0
afaqalii <afaqali4244@gmai.com>
2023-05-08 13:47:45 +0500

    started building my portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,18 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='landing-page'>
+        <div className="sidebar">
+          <div className="divs"></div>
+          <div className="divs"></div>
+          <div className="divs"></div>
+        </div>
+        <div className="body">
+          <div className="header">
+            <h1>Afaq</h1>
+            <h2>Front-end developer</h2>
+          </div>
+        </div>
       </div>
     </>
   )
