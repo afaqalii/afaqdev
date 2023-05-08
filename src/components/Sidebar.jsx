d0e9bf7f840973f3f4e544a4014a8386f58a39f8
afaqalii <afaqali4244@gmai.com>
2023-05-08 19:52:43 +0500

    Completed all the features of navbar, Nothing is left to be done more in navbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ const Sidebar = () => {
   const MenuItems = menuItems.map((menuItem) => {
     const { item } = menuItem
       return (
-        <li className='li relative lg:static list-none text-[--light-gray] capitalize p-2 hover:text-[--dark-blue] after:content-[""] after:absolute after:bottom-0 after:left-[8px] after:bg-[--light-gray] after:opacity-10 after:h-[1px] after:w-[100%]'>
+        <li className='li relative lg:static list-none text-[--light-gray] font-bold capitalize p-2 hover:text-[--dark-blue] after:content-[""] after:absolute after:bottom-0 after:left-[8px] after:bg-[--light-gray] after:opacity-10 after:h-[1px] after:w-[100%]'>
           <a href="#" className='tracking-wide'>{item}</a>
         </li>
       )
@@ -18,8 +18,8 @@ const Sidebar = () => {
     <div className='fixed w-[100%] flex bg-[--dark-black] items-center justify-between px-3 py-4 lg:flex-col  lg:min-h-full  lg:w-[260px]'>
        {/* sidebar Profile Photo section */}
        <div>
-          <div className="hidden border-solid w-[160px] border-8 border-[#343A40] rounded-[50%]  lg:flex flex-col items-center  mt-6 ">
-             <img src={MyPhoto} className='flex rounded-[50%]' alt="My Photo" />
+          <div className="hidden border-solid w-[160px] h-[170px] border-8 border-[#343A40] rounded-[50%]  lg:flex flex-col items-center  mt-6 ">
+             <img src={MyPhoto} className='flex rounded-[50%] w-[100%] h-[100%] object-cover ' alt="My Photo" />
           </div>
           <h1 className="text-[21px] lg:mt-3 font-bold text-white text-center">Muhammad Afaq</h1>
        </div>
